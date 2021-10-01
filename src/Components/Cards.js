7f2a520e1d0d2d16188f17326c47f37e725afbf0
Vishal Kumar Singh <vishal.singh@hummingwave.com>
2021-10-01 10:14:52 +0530

    fixed one bug

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -11,7 +11,7 @@ const Cards = (props) => {
     return (
         <>
             <div className='card' key={props.id} id={props.id}>
-                <h1 style={{ marginLeft: '10px', marginBottom: '0px', height: '36px' }}>{props.title}</h1>
+                <h1 style={{ marginLeft: '10px', marginBottom: '0px', height: '36px', overflowX: 'hidden' }}>{props.title}</h1>
                 <br />
                 <div style={{ overflow: 'auto', height: "96px" }}>
                     <p style={{ marginLeft: '10px', marginTop: '0px' }}>{props.content}</p><br />
