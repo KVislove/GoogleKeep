e928b5a57a0e422d87dd3aacc8dab548bd3291b7
vishal.hummingwave <vishal.hummingwave@lendingkart.com>
2022-04-23 17:03:21 +0530

    added login form and logout functionality

--- src/App.js
+++ src/App.js
@@ -1,56 +1,17 @@
 import React, { useEffect, useState } from 'react'
-import Cards from './Components/Cards';
-import CreateNote from './Components/CreateNote';
-import Footer from './Components/Footer';
-import Header from './Components/Header';
-import { Service } from './Components/Service';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './Components/Dashboard/Dashboard';
+import Login from './Components/Login/Login';
+import Logout from './Components/Logout/Logout';
 
 function App() {
 
-  const [notes, setNotes] = useState([])
-
-  useEffect(()=>{
-    fetchNotes()
-  },[])
-
-  const addNote = (note) => {
-    Service.insertNote(note).then(data=>{
-      console.log(data.data);
-      fetchNotes()
-    }).catch(err=>{
-      console.log(err)
-    })
-  }
-
-  const deleteNote = (id) => {
-    Service.deleteNote(id).then(data=>{
-      console.log(data.data)
-      fetchNotes()
-    }).catch(error=>{
-      console.log(error)
-    })
-  }
-
-  const fetchNotes=()=>{
-    Service.fetchNotes().then(data=>{
-      setNotes(data.data)
-    }).catch(error=>{
-      console.log(error)
-    })
-  }
-
   return (
     <>
-      <Header />
-      <CreateNote passNote={addNote} />
-      {notes.length !== 0 && <div className='cards'>
-        {
-          notes.map((val) => {
-            return <Cards key={val.id} id={val.id} title={val.title} content={val.content} del={deleteNote} />
-          })
-        }
-      </div>}
-      <Footer />
+      <Routes>
+        <Route path='/GoogleKeep' element={<Login/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+      </Routes>
     </>
   );
 }
---
+++ src/Components/Logout/Logout.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDropdown from "react-dropdown";
+import 'react-dropdown/style.css';
+import { useNavigate } from "react-router-dom";
+import { KvLocalStorage } from "../../Utils/KvLocalStorage";
+import "./logout.css"
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const Logout=(props)=>{
+    const options=[{ value: 'logout', label: 'logout', className: 'logout-option' },];
+    const navigate= useNavigate()
+    const logout=()=>{
+        KvLocalStorage.clear()
+        navigate('/GoogleKeep')
+    }
+    return(
+        <>
+            <div style={{display:"flex", flexDirection:"row"}}>
+            <AccountCircleIcon className="circle-icon"/>
+            <ReactDropdown controlClassName="dropdown" menuClassName="menu" arrowClassName="arrow" options={options} value={props.username.toUpperCase()} onChange={()=>logout()}/>  
+            </div>
+        </>
+    )
+}
+
+export default Logout;
